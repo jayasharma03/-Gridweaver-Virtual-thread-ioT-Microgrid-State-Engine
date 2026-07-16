7b18b1a64051244bf0ad0a03a62965f0ec5f5897
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-16 22:52:53 +0530

    Added backend setup

---
+++ OneDrive/Desktop/Gridweaver/gridweaver-backend/src/main/java/com/gridweaver/backend/GridweaverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gridweaver.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridweaverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridweaverBackendApplication.class, args);
+	}
+
+}
