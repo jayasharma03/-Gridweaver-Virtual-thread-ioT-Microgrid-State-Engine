d8aa9817b99d99ea5920d4c73a1707c1185e4fd9
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-16 23:17:27 +0530

    Backend setup

---
+++ src/test/java/com/gridweaver/backend/GridweaverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridweaver.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridweaverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
