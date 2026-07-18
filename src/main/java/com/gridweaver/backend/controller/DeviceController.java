16d401c71e3c2afaf614cb950cb9c983b8830ed2
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 22:00:37 +0530

    Implemented PUT API to update device details

--- src/main/java/com/gridweaver/backend/controller/DeviceController.java
+++ src/main/java/com/gridweaver/backend/controller/DeviceController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,14 @@ public class DeviceController {
 	    public List<Device> getAllDevices() {
 	        return service.getAllDevices();
 	    }
+	   
+	    @PutMapping("/{id}")
+	    public Device updateDevice(
+	            @PathVariable Long id,
+	            @RequestBody Device device) {
 
+	        return service.updateDevice(id, device);
+	    }
 	    
 	
 	
