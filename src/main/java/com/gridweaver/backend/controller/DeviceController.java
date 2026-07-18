10fdbe6f7eaef67baf8442deec069ffb9943f532
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 21:39:32 +0530

    Implemented GET API for devices

--- src/main/java/com/gridweaver/backend/controller/DeviceController.java
+++ src/main/java/com/gridweaver/backend/controller/DeviceController.java
@@ -1,6 +1,9 @@
 package com.gridweaver.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,11 @@ public class DeviceController {
 	    @PostMapping
 	    public Device createDevice(@RequestBody Device device) {
 	        return service.saveDevice(device);
+	    }
+	    
+	    @GetMapping
+	    public List<Device> getAllDevices() {
+	        return service.getAllDevices();
 	    }
 
 	    
