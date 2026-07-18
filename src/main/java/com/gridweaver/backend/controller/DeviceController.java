070598ad1184d1226742c11ebcd39bf9eb64fd00
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 22:03:44 +0530

    Implemented DELETE API for device removal

--- src/main/java/com/gridweaver/backend/controller/DeviceController.java
+++ src/main/java/com/gridweaver/backend/controller/DeviceController.java
@@ -3,6 +3,7 @@ package com.gridweaver.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,11 @@ public class DeviceController {
 	            @RequestBody Device device) {
 
 	        return service.updateDevice(id, device);
+	    }
+	    
+	    @DeleteMapping("/{id}")
+	    public void deleteDevice(@PathVariable Long id) {
+	        service.deleteDevice(id);
 	    }
 	    
 	
