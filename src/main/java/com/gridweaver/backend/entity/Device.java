8044709844d38c814e5c0791f3cc0c25682fb4ec
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 11:57:11 +0530

    Updated Device entity with fields and annotations

--- src/main/java/com/gridweaver/backend/entity/Device.java
+++ src/main/java/com/gridweaver/backend/entity/Device.java
@@ -1,18 +1,18 @@
 package com.gridweaver.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Device {
 
     @Id
-    private Long id;
-
-    private String deviceId;
-    private String deviceName;
-    private String status;
-    private Double powerOutput;
-    private String location;
-    private String batteryState;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String deviceId;
+	private String status;
+	private Double powerOutput;
+	private String location;
 }
