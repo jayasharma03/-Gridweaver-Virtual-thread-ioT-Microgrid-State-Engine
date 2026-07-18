33c10222d0bbece650fba3209f6bf5bd777f8fde
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 21:36:22 +0530

    Implemented POST API for device creation

--- src/main/java/com/gridweaver/backend/controller/DeviceController.java
+++ src/main/java/com/gridweaver/backend/controller/DeviceController.java
@@ -1,8 +1,51 @@
 package com.gridweaver.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gridweaver.backend.entity.Device;
+import com.gridweaver.backend.service.DeviceService;
+
 @RestController
+@RequestMapping("/devices")
 public class DeviceController {
 
+	
+	
+	 @Autowired
+	    private DeviceService service;
+
+	    @PostMapping
+	    public Device createDevice(@RequestBody Device device) {
+	        return service.saveDevice(device);
+	    }
+
+	    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
