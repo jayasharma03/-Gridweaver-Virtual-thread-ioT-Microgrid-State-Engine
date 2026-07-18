8234f64f95a8e40f68deef4fb513c9b8de8ea44c
Harshada Rahate <harshadarahate20@gmail.com>
2026-07-18 21:25:39 +0530

    Implemented device service methods

--- src/main/java/com/gridweaver/backend/service/DeviceService.java
+++ src/main/java/com/gridweaver/backend/service/DeviceService.java
@@ -1,10 +1,34 @@
 package com.gridweaver.backend.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gridweaver.backend.entity.Device;
+import com.gridweaver.backend.repository.DeviceRepository;
 
 @Service
 public class DeviceService {
+	 @Autowired
+	    private DeviceRepository repository;
 
+	    public Device saveDevice(Device device) {
+	        return repository.save(device);
+	    }
+
+	    public List<Device> getAllDevices() {
+	        return repository.findAll();
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 
