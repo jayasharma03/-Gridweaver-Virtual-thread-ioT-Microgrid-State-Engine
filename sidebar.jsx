7e25e1d7f45241944c070be50b8d05d5d677bb85
Jaya sharma <imsharmajaya@gmail.com>
2026-07-21 23:46:06 +0530

    Add files via upload

---
+++ sidebar.jsx
@@ -0,0 +1,17 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>⚡ GridWeaver</h2>
+
+      <ul>
+        <li>🏠 Dashboard</li>
+        <li>☀ Solar</li>
+        <li>🔋 Battery</li>
+        <li>⚡ Grid</li>
+        <li>⚙ Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
